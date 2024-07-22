790229d00514a0b828edbb36728faa11828787df
FirthR5 <47197502+FirthR5@users.noreply.github.com>
2024-07-21 19:29:00 -0500

    feat(LSP): add "emmet-ls" to config & remove useless angular comments

--- lua/configs/Core/lspconfig.lua
+++ lua/configs/Core/lspconfig.lua
@@ -62,25 +62,38 @@ local servers = {
   --"clangd",
   -- ===================================================
 }
--- local on_attach2 = function(client, bufnr)
---   require("plugins.configs.lspconfig").on_attach(client, bufnr)
---
---   -- refresh codelens when buffer enters and buffer is saved
---   -- insertleave textchanged
---   vim.api.nvim_create_autocmd({ "bufenter", "bufwritepre" }, {
---     buffer = bufnr,
---     callback = vim.lsp.codelens.refresh,
---   })
---
---   -- trigger code lens
---   vim.api.nvim_exec_autocmds("user", { pattern = "lspattached" })
--- end
---
--- capabilities.textDocument.foldingRange = {
---   dynamicRegistration = false,
---   lineFoldingOnly = true,
--- }
 
+-- https://github.com/aca/emmet-ls
+local configs = require "lspconfig.configs"
+local capabilities2 = vim.lsp.protocol.make_client_capabilities()
+capabilities2.textDocument.completion.completionItem.snippetSupport = true
+
+lspconfig.emmet_ls.setup {
+  -- on_attach = on_attach,
+  capabilities = capabilities2,
+  filetypes = {
+    "css",
+    "eruby",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "less",
+    "sass",
+    "scss",
+    "svelte",
+    "pug",
+    "typescriptreact",
+    "vue",
+  },
+  init_options = {
+    html = {
+      options = {
+        -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+        ["bem.enabled"] = true,
+      },
+    },
+  },
+}
 -- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -92,84 +105,6 @@ end
 
 ----
 
--- local cmd = {
---   "ngserver",
---   "--stdio",
---   "--tsProbeLocations",
---   project_library_path,
---   "--ngProbeLocations",
---   project_library_path,
--- }
--- lspconfig.angularls.setup {
---   cmd = cmd,
---   on_attach = on_attach,
---   on_init = on_init,
---   capabilities = capabilities,
--- }
--- typescript
--- lspconfig.tsserver.setup {
---   on_attach = on_attach2,
---   on_init = on_init,
---   capabilities = capabilities,
---   filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
---   cmd = { "typescript-language-server.cmd", "--stdio" },
--- }
---
--- local project_library_path = mason_packages .. "/angular-language-server/node_modules/.bin/ngserver"
--- local cmd =
---   { "ngserver", "--stdio", "--tsProbeLocations", project_library_path, "--ngProbeLocations", project_library_path }
---
--- require("lspconfig").angularls.setup {
---   cmd = cmd,
---   on_new_config = function(new_config, new_root_dir)
---     new_config.cmd = cmd
---   end,
--- }
--- require("lspconfig").angularls.setup {}
-
 -- -- @server angular-language-server
 -- -- https://github.com/yavuloh/nvim_angular/blob/main/lua/custom/configs/lspconfig.lua#L47
--- local angular_language_server_path = mason_packages .. "/angular-language-server/node_modules/.bin/ngserver"
--- local typescript_language_server_path = mason_packages .. "/typescript-language-server/node_modules/.bin/tsserver"
--- local anigular_logs_path = vim.fn.stdpath "state" .. "/angularls.log"
--- local node_modules_global_path = "/usr/local/lib/node_modules"
---
--- local ngls_cmd = {
---   -- "node",
---   angular_language_server_path,
---   "--stdio",
---   "--tsProbeLocations",
---   typescript_language_server_path,
---   "--ngProbeLocations",
---   node_modules_global_path,
---   "--includeCompletionsWithSnippetText",
---   "--includeAutomaticOptionalChainCompletions",
---   "--logToConsole",
---   "--logFile",
---   anigular_logs_path,
--- }
--- local util = require "lspconfig.util"
--- lspconfig.angularls.setup {
---   cmd = ngls_cmd,
---   on_attach = on_attach,
---   on_init = on_init,
---   capabilities = capabilities,
---   on_new_config = function(new_config, _)
---     new_config.cmd = ngls_cmd
---   end,
---   -- filetypes = { "typescript", "html", "typescriptreact", "typescript.tsx" },
---   root_dir = util.root_pattern ".git", --,"angular.json", "project.json"),
--- }
-
--- local languageServerPath = "/home/user/.fnm/node-versions/v12.22.12/installation/lib/node_modules"
--- local cmd = {"ngserver", "--stdio", "--tsProbeLocations", languageServerPath , "--ngProbeLocations", languageServerPath}
---
--- lspconfig.angularls.setup({
---   on_attach = on_attach,
---   capabilities = .capabilities,
---   cmd = cmd,
---   on_new_config = function(new_config,new_root_dir)
---     new_config.cmd = cmd
---   end,
--- })
 -- https://github.com/neovim/nvim-lspconfig/issues/1155
