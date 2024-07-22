f56aec2499316d7c8b0b16fdd022df01fb996418
FirthR5 <47197502+FirthR5@users.noreply.github.com>
2024-07-21 19:30:30 -0500

    refactor(web-tools): move and make web-tools plugin works

--- lua/configs/EXT/web_tools.lua
+++ lua/configs/EXT/web_tools.lua
@@ -1,10 +1,10 @@
-require("web-tools").setup {
+local options = {
   keymaps = {
     rename = nil, -- by default use same setup of lspconfig
     repeat_rename = ".", -- . to repeat
   },
   hurl = { -- hurl default
-    show_headers = false, -- do not show http headers
+    show_headers = true, -- do not show http headers
     floating = false, -- use floating windows (need guihua.lua)
     json5 = false, -- use json5 parser require json5 treesitter
     formatters = { -- format the result by filetype
@@ -12,4 +12,12 @@ require("web-tools").setup {
       html = { "prettier", "--parser", "html" },
     },
   },
+}
+
+return {
+  "ray-x/web-tools.nvim",
+  lazy = false,
+  config = function()
+    require("web-tools").setup(options)
+  end,
 }
