8b529a7cb9f3ed651020a5fdb7b01b53faf1cf6c
FirthR5 <47197502+FirthR5@users.noreply.github.com>
2024-07-22 19:00:40 -0500

    feat(trouble): Add invoke keybindings

--- lua/configs/EXT/trouble.lua
+++ lua/configs/EXT/trouble.lua
@@ -4,6 +4,40 @@ local dependencias = {
     opts = {},
   },
 }
+
+local the_keys = {
+  {
+    "<leader>xx",
+    "<cmd>Trouble diagnostics toggle<cr>",
+    desc = "Diagnostics (Trouble)",
+  },
+  {
+    "<leader>xX",
+    "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+    desc = "Buffer Diagnostics (Trouble)",
+  },
+  {
+    "<leader>cs",
+    "<cmd>Trouble symbols toggle focus=false<cr>",
+    desc = "Symbols (Trouble)",
+  },
+  {
+    "<leader>cl",
+    "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+    desc = "LSP Definitions / references / ... (Trouble)",
+  },
+  {
+    "<leader>xL",
+    "<cmd>Trouble loclist toggle<cr>",
+    desc = "Location List (Trouble)",
+  },
+  {
+    "<leader>xQ",
+    "<cmd>Trouble qflist toggle<cr>",
+    desc = "Quickfix List (Trouble)",
+  },
+}
+
 function Configuration()
   require("trouble").setup()
 end
@@ -12,8 +46,9 @@ local M = {
   "folke/trouble.nvim",
   cmd = { "Trouble", "TodoTrouble" },
   dependencies = dependencias,
+  keys = the_keys,
   config = function()
-    Cconfiguration()
+    Configuration()
   end,
 }
 
