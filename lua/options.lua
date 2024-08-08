61d7e30b232a4740939681ea28dda48447c3ab12
FirthR5 <fernando.fernandezrios@hotmail.com>
2024-08-07 19:16:58 -0500

    refactor: options (add some style)

--- lua/options.lua
+++ lua/options.lua
@@ -1,30 +1,42 @@
 require "nvchad.options"
-
+-- ==================================================================
 -- Vim Variables
+-- ==================================================================
 local o = vim.o
 local opt = vim.opt
 local cmd = vim.cmd
+-- ==================================================================
 
+-- ==================================================================
+-- Options
+-- ==================================================================
 o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+--cmd.colorscheme("habamax")-- slate, habamax
+cmd.highlight { "link", "Warning", "Error" }
+opt.wrap = true
+opt.clipboard = "unnamedplus"
+
+-- https://neovim.io/doc/user/lua-guide.html
+-- ==================================================================
 -- Numbers and Tabs
+-- ==================================================================
 o.expandtab = true
 o.relativenumber = true
 opt.smarttab = true
+-- ==================================================================
 
---cmd.colorscheme("habamax")-- slate, habamax
-cmd.highlight { "link", "Warning", "Error" }
--- https://neovim.io/doc/user/lua-guide.html
-
+-- ==================================================================
 -- Indent
+-- ==================================================================
 opt.autoindent = true
 opt.smartindent = true
 -- vim.opt.cmdheight = 0
 -- vim.opt.laststatus = 0
 -- vim.opt.inccommand = "split"
-opt.smarttab = true
 opt.breakindent = true
-opt.wrap = true
-opt.clipboard = ""
+-- ==================================================================
+
+-- ==================================================================
 -- vim.o.shell = "pwsh" --"C:/Program Files/PowerShell/7/pwsh.exe"
 
 --require "configs.vim_configs"
@@ -32,3 +44,4 @@ opt.clipboard = ""
 -- https://stackoverflow.com/questions/1676632/whats-a-quick-way-to-comment-uncomment-lines-in-vim
 --https://hatchjs.com/neovim-copy-to-system-clipboard/
 -- https://stackoverflow.com/questions/2362914/fold-function-in-vim
+-- ==================================================================
