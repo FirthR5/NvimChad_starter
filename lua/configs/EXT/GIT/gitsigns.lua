74c3ad45e2da6c837475c45c0086b77b803e9bf9
FirthR5 <47197502+FirthR5@users.noreply.github.com>
2024-08-15 22:23:56 -0500

    refactor: move gitsign & lazygit to EXT/GIT subfolder

--- lua/configs/EXT/GIT/gitsigns.lua
+++ lua/configs/EXT/GIT/gitsigns.lua
@@ -9,6 +9,16 @@ local M = {
       changedelete = { text = "~" },
       untracked = { text = "┆" },
     },
+    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+      delay = 1000,
+      ignore_whitespace = false,
+      virt_text_priority = 100,
+    },
+    current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
+
     on_attach = function(bufnr)
       local keybinds = require("prefs.keymap.plug.EXT_MISC").gitsigns
       vim.keymap.set(
@@ -26,6 +36,7 @@ local M = {
       )
     end,
   },
+  -- toggle_current_line_blame = true
 }
 
 return M
