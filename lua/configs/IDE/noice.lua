6f36f67c7ab0d0f2c27be32922108ec2de3f65f4
FirthR5 <47197502+FirthR5@users.noreply.github.com>
2024-08-15 22:27:14 -0500

    chore: notify & noice little change settings (a little refactor)

--- lua/configs/IDE/noice.lua
+++ lua/configs/IDE/noice.lua
@@ -12,6 +12,12 @@ local options = {
       signature = { enabled = false }, -- <-- HERE!
     },
   },
+  progress = {
+    enabled = true,
+  },
+  hover = {
+    enabled = false,
+  },
   presets = {
     bottom_search = true, -- use a classic bottom cmdline for search
     command_palette = true, -- position the cmdline and popupmenu together
--- lua/configs/IDE/nvim_notify.lua
+++ lua/configs/IDE/nvim_notify.lua
@@ -1,6 +1,10 @@
 local M = {}
 
 local options = {
+  -- Animation style
+  stages = "fade_in_slide_out",
+  -- Default timeout for notifications
+  timeout = 1200,
   background_colour = "#58afe8",
 }
 
