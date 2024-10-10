84754294416dcbf2e012fbaf8ce384ef2ccd37c4
FirthR5 <47197502+FirthR5@users.noreply.github.com>
2024-10-10 11:35:03 -0500

    refactor: get time_spend_keys by keymap file (timespent)

--- lua/configs/IDE/timespent.lua
+++ lua/configs/IDE/timespent.lua
@@ -1,6 +1,8 @@
+local time_spend_keys = require("prefs.keymap.plug.IDE_Misc").timespent
+
 return {
   "quentingruber/timespent.nvim",
   keys = {
-    { "<leader>ts", "<cmd>:ShowTimeSpent<cr>", mode = { "n" }, desc = "Show time spent" },
+    { time_spend_keys[1], time_spend_keys[2], mode = { "n" }, desc = "Show time spent" },
   },
 }
