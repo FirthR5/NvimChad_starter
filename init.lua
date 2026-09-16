ce0cab482df55a67a8ef13ce8f8a97d2c76f52ef
FirthR5 <47197502+FirthR5@users.noreply.github.com>
2026-09-16 12:19:12 -0500

    fix: update nvchad

--- init.lua
+++ init.lua
@@ -30,6 +30,21 @@ require("lazy").setup({
       end)
     end,
   },
+  {
+    "nvchad/base46",
+    branch = "v2.5",
+    build = function()
+      require("base46").load_all_highlights()
+    end,
+  },
+  {
+    "nvchad/ui",
+    lazy = false,
+    branch = "v2.5",
+    config = function()
+      require "nvchad"
+    end,
+  },
   { import = "plugins" },
 }, lazy_config)
 
